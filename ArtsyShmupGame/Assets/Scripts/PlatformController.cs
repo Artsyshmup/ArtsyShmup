c9e838a8b268771d220e13078aa57056b5b52377
Raul Estrada <raulestr@live.unc.edu>
2015-02-25 12:39:39 -0500

    Spawned platforms. When the user walks to the second half of a platform, the new platform is instantiated. The platforms have gaps into each other and the player must jump to go from a platform to the next one

---
+++ ArtsyShmupGame/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+	public GameObject spawnedElement;
+	private static int previous_platform_id = 0;
+
+	private int platform_id;
+	private GameObject spawningElement;
+
+	// Use this for initialization
+	void Awake () {
+		this.platform_id = PlatformController.previous_platform_id + 1;
+		PlatformController.previous_platform_id = this.platform_id;
+	}
+
+	public void SpawnNewPlatform()
+	{
+		Vector2 newPosition = transform.position;
+		Debug.Log (transform.localScale.x);
+		newPosition.x += transform.localScale.x + GameObject.Find ("Player").GetComponent<PlayerController>().jumpHeight /3;
+		Debug.Log ("New position x: " + newPosition.x);
+		Instantiate (spawnedElement, newPosition, Quaternion.identity);
+	}
+}
---
+++ ArtsyShmupGame/Assets/Scripts/TriggerPlatformCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerPlatformCollider : MonoBehaviour {
+	private PlatformController platformController;
+	private BoxCollider2D collider;
+
+	void Start()
+	{
+		collider = GetComponent<BoxCollider2D> ();
+		platformController = transform.parent.gameObject.GetComponent<PlatformController> ();
+		transform.position = new Vector2 (
+			transform.parent.position.x,
+			transform.parent.position.y + collider.size.y/2);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			Debug.Log ("SPAWN!");
+			platformController.SpawnNewPlatform();
+			Destroy (gameObject);
+		}
+	}
+}
