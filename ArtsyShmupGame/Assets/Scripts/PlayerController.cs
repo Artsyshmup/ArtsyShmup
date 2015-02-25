9b35ce4e09ddbd29770c901127ea7c24dec10cbb
Raul Estrada <raulestr@live.unc.edu>
2015-02-24 19:21:57 -0500

    Initialization of the basic structure of the first level: Cube represents the player and the rectangle is our first platform. Added two directional lights: a main one and a fill one. Two main scripts for the player: one to handle Unity functions and user input and another to handle physics. Right now the player can only move to the right and to the left

---
+++ ArtsyShmupGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+	// Public variable to control the speed of the player
+	public float speed = 4;
+
+	private PlayerPhysics playerPhysics;
+
+	// This function will be executed when it's loaded.
+	void Awake () {
+		this.playerPhysics = GetComponent<PlayerPhysics> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		Vector2 moveTo = new Vector2 (horizontal, 0) * speed;
+
+		playerPhysics.Move (moveTo);
+	}
+}
