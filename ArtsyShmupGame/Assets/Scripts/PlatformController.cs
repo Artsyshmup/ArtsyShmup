ecdb3024d925990cf3b4f5d5abbd0d4ff592f521
Raul Estrada <raulestr@live.unc.edu>
2015-02-25 14:45:23 -0500

    Destroying old platforms: a new platform is created when the player goes to the second half of the last platform. The game will only keep 5 previous platforms. Once a new platform is created, the platform to be destroyed will experience the effects of gravity and fall in space (and when it goes off screen the game object will be destroyed)

--- ArtsyShmupGame/Assets/Scripts/PlatformController.cs
+++ ArtsyShmupGame/Assets/Scripts/PlatformController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlatformController : MonoBehaviour {
 	public GameObject spawnedElement;
+	public int destroyDelay = 3;
 	private static int previous_platform_id = 0;
 
 	private int platform_id;
@@ -12,16 +13,29 @@ public class PlatformController : MonoBehaviour {
 	void Awake () {
 		this.platform_id = PlatformController.previous_platform_id + 1;
 		PlatformController.previous_platform_id = this.platform_id;
+
+		gameObject.name = "Platform" + this.platform_id;
+
+		if (this.platform_id >= 6) {
+			StartCoroutine("DestroyOldPlatform");
+		}
+	}
+
+	IEnumerator DestroyOldPlatform()
+	{
+		Debug.Log("DESTROY OLD PLATFORM");
+		GameObject oldPlatform = GameObject.Find("Platform" + (this.platform_id-5));
+		oldPlatform.rigidbody2D.isKinematic = false;
+		yield return new WaitForSeconds(destroyDelay);
+		Destroy (oldPlatform);
 	}
 
 	public void SpawnNewPlatform()
 	{
 		Vector2 newPosition = transform.position;
-		Debug.Log (transform.localScale.x);
 		float jumpHeight = GameObject.Find ("Player").GetComponent<PlayerController> ().jumpHeight;
 		newPosition.x += transform.localScale.x +  jumpHeight/3;
 		newPosition.y += Random.Range (-jumpHeight/2, jumpHeight/4);
-		Debug.Log ("New position x: " + newPosition.x);
 		Instantiate (spawnedElement, newPosition, Quaternion.identity);
 	}
 }
