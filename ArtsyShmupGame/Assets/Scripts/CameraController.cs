cf8f1a90586fc83c04f3aaca3fbdfe3cb0ffd95d
Raul Estrada <raulestr@live.unc.edu>
2015-03-12 15:34:46 -0400

    Added a camera controller that gets the position of the mouse in the game screen. Then, it compares this position to the position of the player to see if the mouse if at the top-left, top-right, bottom-right or bottom-left quadrant of the screen. I do this so later we can move the camera according to the mouse position like David said in the last meeting

---
+++ ArtsyShmupGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float horizontalSpeed = 2f;
+	public float verticalSpeed = 2f;
+
+	private float camRayLength = 100f;
+	
+	// Update is called once per frame
+	void Update () {
+		//This will get the position of the mouse
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		//We check that there was a hit
+		if (Physics.Raycast (camRay, out hit, camRayLength)) {//The mouse was in the screen and we have the coordinates
+			float horizontal = (hit.point.x > transform.position.x)? 1 : (hit.point.x < transform.position.x) ? -1 : 0;
+			float vertical = (hit.point.y > transform.position.y) ? 1 : (hit.point.y < transform.position.y) ? -1 : 0;
+		}
+	}
+}
