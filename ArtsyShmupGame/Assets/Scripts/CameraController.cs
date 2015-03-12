63ad544b53fca4727a21ab6f2e0e9b480edb5036
Raul Estrada <raulestr@live.unc.edu>
2015-03-12 16:04:58 -0400

    Added camera translation depending on the mouse position. Deleted the CameraFollow.cs script since it's not needed anymore. The CameraController.cs script will take care of following the player and translating the camera according to the mouse position at the same time

--- ArtsyShmupGame/Assets/Scripts/CameraController.cs
+++ ArtsyShmupGame/Assets/Scripts/CameraController.cs
@@ -5,18 +5,31 @@ public class CameraController : MonoBehaviour {
 
 	public float horizontalSpeed = 2f;
 	public float verticalSpeed = 2f;
+	public float smoothing = 5;
+	public Transform target;
+	private Vector3 offset;
 
 	private float camRayLength = 100f;
+
+	void Start(){
+		offset = transform.position - target.position;
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		//This will get the position of the mouse
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
+
+		Vector2 targetCameraPosition = target.position + offset;
 		//We check that there was a hit
 		if (Physics.Raycast (camRay, out hit, camRayLength)) {//The mouse was in the screen and we have the coordinates
 			float horizontal = (hit.point.x > transform.position.x)? 1 : (hit.point.x < transform.position.x) ? -1 : 0;
 			float vertical = (hit.point.y > transform.position.y) ? 1 : (hit.point.y < transform.position.y) ? -1 : 0;
+			Vector2 movement = new Vector2(horizontal * horizontalSpeed, vertical * verticalSpeed);
+			targetCameraPosition += movement;
 		}
+
+		transform.position = Vector2.Lerp (transform.position, targetCameraPosition, smoothing * Time.deltaTime);
 	}
 }
