bc7c7d021fd325fdc938cc0a2f45fc52589b97ba
Raul Estrada <raulestr@live.unc.edu>
2015-03-20 15:13:50 -0400

    Now in the first level 20 platforms will be created once after another. Then, infinite platforms will still be created but the player will go backwards. I still have to place the object, make the level end once the player goes all the way back again.

--- ArtsyShmupGame/Assets/Scripts/TriggerPlatformCollider.cs
+++ ArtsyShmupGame/Assets/Scripts/TriggerPlatformCollider.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TriggerPlatformCollider : MonoBehaviour {
 	private PlatformController platformController;
 	private BoxCollider2D collider;
+	private int parent_id;
 
 	void Start()
 	{
@@ -12,12 +13,23 @@ public class TriggerPlatformCollider : MonoBehaviour {
 		transform.position = new Vector2 (
 			transform.parent.position.x,
 			transform.parent.position.y + collider.size.y/2);
+		parent_id = transform.parent.gameObject.GetComponent<PlatformController> ().platform_id;
+		if (parent_id < PlatformController.platformsPerLevel) {
+			gameObject.tag = "PlatformForward";
+		} else {
+			gameObject.tag = "PlatformBackward";
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player") {
-			platformController.SpawnNewPlatform();
+			if(parent_id == PlatformController.platformsPerLevel){
+				for(int i=PlatformController.oldest_platform_id; i<parent_id; i++){
+					platformController.DestroyOldPlatform(i);
+				}
+			}
+			platformController.SpawnNewPlatform(gameObject.tag=="PlatformForward");
 			Destroy (gameObject);
 		}
 	}
