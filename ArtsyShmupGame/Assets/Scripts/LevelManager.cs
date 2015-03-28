9acfb7e7dcf79555e936e2c15cc9131bb0ed9b7c
Raul Estrada <raulestr@live.unc.edu>
2015-03-27 22:47:25 -0400

    Now the LevelManager is aware of levels. When the player wins a level, the new level starts. So far, the different levels are the same, but the level manager now knows about the current level. When the player loses, he/she will restart at the beginning of that level

--- ArtsyShmupGame/Assets/Scripts/LevelManager.cs
+++ ArtsyShmupGame/Assets/Scripts/LevelManager.cs
@@ -4,9 +4,10 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 	public static LevelManager instance = null;
-	private int level = 1;
+	private static int LEVEL = 1;
 	[HideInInspector]
 	public bool gameOver = false;
+	public float messageDelay = 3f;
 
 	public Image levelPassedImage;
 	public Text levelPassedText;
@@ -28,7 +29,7 @@ public class LevelManager : MonoBehaviour {
 	{
 		PlayerController playerController = GameObject.Find ("Player").GetComponent<PlayerController> ();
 		if (playerController.GetPickupsSize () > 0) { // The player picked up the treasure
-			WinLevel ();
+			StartCoroutine("WinLevel");
 		} else {
 			LoseLevel();
 		}
@@ -39,9 +40,22 @@ public class LevelManager : MonoBehaviour {
 		GameObject.Find ("Player").GetComponent<PlayerController> ().isAlive = false;
 	}
 
-	private void WinLevel(){
+	private IEnumerator WinLevel(){
 		levelPassedImage.color = new Color (141 / 255f, 151 / 255f, 255 / 255f, 1f);
+		levelPassedText.text = "You've passed level " + LevelManager.LEVEL++;
 		levelPassedText.color = new Color (0f, 0f, 0f, 1f);
+		Time.timeScale = 0f;
+		float pauseEndTime = Time.realtimeSinceStartup + messageDelay;
+		while (Time.realtimeSinceStartup < pauseEndTime)
+		{
+			yield return 0;
+		}
+		Time.timeScale = 1;
+		levelPassedImage.color = new Color (141 / 255f, 151 / 255f, 255 / 255f, 0f);
+		levelPassedText.text = "You've passed level " + LevelManager.LEVEL;
+		levelPassedText.color = new Color (0f, 0f, 0f, 0f);
+		PlatformController.total_platform_id = 0;
+		Application.LoadLevel(Application.loadedLevel);
 	}
 
 	private void LoseLevel(){
@@ -50,7 +64,7 @@ public class LevelManager : MonoBehaviour {
 		gameOverText.fontSize = 30;
 		gameOverText.color = new Color (0, 0, 0, 1);
 		replayText.color = new Color (0, 0, 0, 1);
-		Camera.main.GetComponent<LevelManager> ().gameOver = true;
+		gameOver = true;
 	}
 
 	void Update()
