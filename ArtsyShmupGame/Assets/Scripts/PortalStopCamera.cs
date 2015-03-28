1ee71e800158de91dfad1173e1ec1823dbffd6ce
Raul Estrada <raulestr@live.unc.edu>
2015-03-28 00:03:33 -0400

    Now the camera will stop moving in the x direction when the portal in the end of the level becomes visible

---
+++ ArtsyShmupGame/Assets/Scripts/PortalStopCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalStopCamera : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (renderer.isVisible) {
+			Camera.main.GetComponent<CameraController>().disablePointerFeature();
+		}
+	}
+}
