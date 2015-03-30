e9ff4685fa254b8e0cc477d3bda51f338ca451be
Raul Estrada <raulestr@live.unc.edu>
2015-03-29 23:20:17 -0400

    Now the enemy shooter shoots at the player every x amount of time. Right now it's every 3 seconds

---
+++ ArtsyShmupGame/Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour {
+	public float timeBetweenBullets = 3f;
+	public float range = 100f;
+
+	float timer;
+	Ray2D shootRay;
+	RaycastHit2D shootHit;
+	int shootableMask;
+	ParticleSystem gunParticles;
+	LineRenderer gunLine;
+	Light gunLight;
+	float effectsDisplayTime = .2f;
+
+	private Vector3 targetPoint;
+	
+	// Use this for initialization
+	void Awake () {
+		shootableMask = LayerMask.GetMask ("Player");
+		gunParticles = GetComponent<ParticleSystem> ();
+		gunLine = GetComponent<LineRenderer> ();
+		gunLight = GetComponent<Light> ();
+	}
+	
+	/// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= timeBetweenBullets && Time.timeScale != 0) {
+			targetPoint = GameObject.Find("Player").transform.position;
+			Shoot();
+		}
+		if (timer >= timeBetweenBullets * effectsDisplayTime) {
+			DisableEffects();
+		}
+	}
+	
+	void DisableEffects()
+	{
+		gunLine.enabled = false;
+		gunLight.enabled = false;
+	}
+	
+	void Shoot()
+	{
+		timer = 0f;
+		
+		gunLight.enabled = true;
+		
+		gunParticles.Stop ();
+		gunParticles.Play ();
+		
+		gunLine.enabled = true;
+		gunLine.SetPosition (0, transform.position);
+		
+		shootRay.origin = transform.position;
+		if (targetPoint == null) {
+			shootRay.direction = transform.forward;
+		} else {
+			shootRay.direction = targetPoint - transform.position;
+		}
+		shootHit = Physics2D.Raycast (shootRay.origin, shootRay.direction, range, shootableMask);
+		if (shootHit.collider!=null) {
+			PlayerController playerHit = shootHit.collider.GetComponent<PlayerController>();
+			if(playerHit!=null){
+				playerHit.TakeDamage();
+			}
+			gunLine.SetPosition (1, shootHit.point);
+		} else {
+			gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+		}
+	}
+}
