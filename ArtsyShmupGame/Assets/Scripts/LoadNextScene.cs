d2264fb5f9959a53dba83120c7675078dfefb12b
Jared Beckham <jtbeckha@gmail.com>
2015-04-06 17:18:10 -0400

    added menu screen

---
+++ ArtsyShmupGame/Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextScene : MonoBehaviour {
+	public string nextSceneName;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space"))
+			Application.LoadLevel (nextSceneName);
+	}
+}
