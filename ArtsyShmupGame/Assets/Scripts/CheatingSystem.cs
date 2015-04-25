5952d76cec8c86e285634571f3614cf1caa1bf68
Raul Estrada <raulestradaballesteros@gmail.com>
2015-04-25 13:26:31 +0200

    Added cheating system. If the player presses the P key, all the enemies currently alive will be automatically killed. The key P was chosen because it's one of the keys furthest away from the keys A, W, D, which the player can use to move the main character. Also, the player can only use the cheating system three times in each level. Users don't know about the cheating system, but if they discover it they can certainly use it

---
+++ ArtsyShmupGame/Assets/Scripts/CheatingSystem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheatingSystem : MonoBehaviour {
+	public int available;
+	private int left;
+
+	/// <summary>
+	/// The number of times the player can still use the cheating system is initially the number
+	/// set in the Unity inspector
+	/// </summary>
+	void Awake()
+	{
+		this.left = available;
+	}
+
+	/// <summary>
+	/// If the user presses the key P and has still the option of using the cheating system (left is greater than 0),
+	/// then all the enemies will be destroyed.
+	/// </summary>
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.P) && left > 0) {
+			DestroyEnemies(GameObject.FindGameObjectsWithTag("NormalEnemy"));
+			DestroyEnemies(GameObject.FindGameObjectsWithTag("ShootingEnemy"));
+			DestroyEnemies(GameObject.FindGameObjectsWithTag("PhantomEnemy"));
+			DestroyEnemies(GameObject.FindGameObjectsWithTag("Boss"));
+			left--;
+		}
+	}
+
+	/// <summary>
+	/// Destroies the enemies in the array passed as a parameter.
+	/// </summary>
+	/// <param name="enemies">Enemies.</param>
+	private void DestroyEnemies(GameObject[] enemies)
+	{
+		foreach (GameObject gameObject in enemies){
+			EnemyHealth health = gameObject.GetComponent<EnemyHealth>();
+			health.Die();
+		}
+	}
+}
